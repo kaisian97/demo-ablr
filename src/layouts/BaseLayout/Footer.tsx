1339e21e02838f67b41b933a97fdc67ddc8e786a
KS <kaisian.wong@up-devlabs.io>
2022-03-24 03:48:32 +0800

    Fix style

--- src/layouts/BaseLayout/Footer.tsx
+++ src/layouts/BaseLayout/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = (props: FooterProps) => {
     shallow
   );
   return (
-    <div className="bg-white sticky bottom-0 min-h-[70px] px-4 py-2 shadow-md border-t">
+    <div className="bg-white sticky bottom-0 md:min-h-[70px] px-4 py-4 md:py-2 shadow-md border-t">
       <div className="flex flex-col text-center space-y-3 md:space-y-0 md:flex-row items-center justify-between h-full">
         <div className="text-xs text-slate-400">© 2022 Demo</div>
         <div className="text-xs text-slate-400">
