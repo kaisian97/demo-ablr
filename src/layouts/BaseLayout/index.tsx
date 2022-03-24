f1a1876932c5756c60d2800913d675802d5f48ee
KS <kaisian.wong@up-devlabs.io>
2022-03-25 01:50:41 +0800

    Refine toast style

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -8,7 +8,10 @@ type BaseLayoutProps = {};
 const BaseLayout = ({ children }: PropsWithChildren<BaseLayoutProps>) => {
   return (
     <div className="max-w-screen-2xl m-auto flex flex-col min-h-screen">
-      <Toaster position="top-right" />
+      <Toaster
+        containerClassName="w-2/3 ml-auto md:w-auto mt-6"
+        position="top-right"
+      />
       <NavBar />
       <div className="p-4 md:p-8 flex-1">{children}</div>
       <Footer />
